501d72fbe7d1f82ccb26f7c1fd4502c7242fbef4
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-11 00:37:28 -0400

    Product model added

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+namespace WebService.Models {
+    public class Product {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Sku { get; set; }
+        public decimal Price { get; set; }
+    }
+}
