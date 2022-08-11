dc68957cab27ce5aa77cd7a50409d95bc0ff17e1
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-11 00:04:01 -0400

    Address Model added

---
+++ Models/Address.cs
@@ -0,0 +1,11 @@
+namespace WebService.Models {
+    public class Address {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Street1 { get; set; }
+        public String Street2 { get; set; }
+        public String City { get; set; }
+        public String Country { get; set; }
+        public String ZipCode { get; set; }
+    }
+}
