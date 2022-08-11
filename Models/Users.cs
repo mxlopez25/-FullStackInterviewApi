ee052c6ece61c0d26005eb225abe2af6edefdbc9
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-11 00:00:55 -0400

    User model added

---
+++ Models/Users.cs
@@ -0,0 +1,10 @@
+namespace WebService.Models {
+    public class Users {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
