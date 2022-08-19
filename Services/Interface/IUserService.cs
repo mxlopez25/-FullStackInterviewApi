04991cf3bd10095721a7689bd3cc693aa9c930d8
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-19 18:27:31 -0400

    IUserService inteface added

---
+++ Services/Interface/IUserService.cs
@@ -0,0 +1,11 @@
+using WebService.Models;
+
+namespace WebService.Interfaces {
+    public interface IUserService {
+        Task<List<Users>> All();
+        Task<Users> Get(int Id);
+        Task<Users> Create(Users user);
+        Task<Users> Update(Users user);
+        Task<bool> Delete(int Id);
+    }
+}
