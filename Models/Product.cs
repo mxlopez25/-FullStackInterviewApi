08052579471434b98126f03344020b0ae337f5db
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-19 16:40:04 -0400

    Update in Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace WebService.Models {
     public class Product {
         public int Id { get; set; }
         public String Name { get; set; }
         public String Sku { get; set; }
+        
+        [Column(TypeName = "decimal(18,4)")]
         public decimal Price { get; set; }
     }
 }
