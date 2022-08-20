6bf2efd79a20e4fe4ad373d975c17b4fb0c96324
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-20 00:58:43 -0400

    UserController updated

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,25 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using WebService.Interfaces;
 using WebService.Models;
 
 namespace WebService.Controllers {
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase {
-        public UserController() {
-
+        private readonly IUserService _userService;
+        public UserController(IUserService userService) {
+            _userService = userService;
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id) {
-            var u = new Users();
-            u.Id = 1;
-            u.FirstName = "Maximo";
-            u.LastName = "Lopez";
-            u.Email = "maximo.lopez@test.com";
-            u.Username = "mlopez";
-            u.Password = "Test123";
-            u.Status = UserStatus.Inactive;
-            return Ok(u);
+            var u = await _userService.Get(id);
+            return (u != null) ? Ok(u) : NotFound("No users found.");
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody]Users user) {
+            if(user.isValid()) {
+                var u = await _userService.Create(user);
+                return Ok(u);
+            } else {
+                return BadRequest(user);
+            }
         }
     }
 }
