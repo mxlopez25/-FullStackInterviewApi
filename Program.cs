978871902b1951927d606ea38a1c67cd4b4ab373
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-20 00:58:26 -0400

    DataContext addded to DI

--- Program.cs
+++ Program.cs
@@ -4,13 +4,16 @@ using WebService.Interfaces;
 using WebService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<DataContext>();
 
 // Add Services
 builder.Services.AddScoped<IUserService, UserService>();
