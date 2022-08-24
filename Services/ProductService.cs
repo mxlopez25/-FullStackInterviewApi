40f6062b1be1ce2f012464a6724c2ed17c08d784
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-23 22:02:35 -0400

    ProductService implemented

---
+++ Services/ProductService.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using WebService.Helpers;
+using WebService.Models;
+using WebService.Models.Entities;
+using WebService.Services.Interfaces;
+
+namespace WebService.Services {
+    public class ProductService : IProductService
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductService(DataContext dataContext) {
+            _dataContext = dataContext;
+        }
+
+        public async Task<EProduct?> All()
+        {
+            var products = await _dataContext.Products.ToListAsync();
+            return new EProduct(products);
+        }
+
+        public async Task<EProduct?> Create(Product product)
+        {
+            _dataContext.Products.Add(product);
+            await _dataContext.SaveChangesAsync();
+            return new EProduct(product);
+        }
+
+        public Task<bool> Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<EProduct?> Get(int Id)
+        {
+            var product = await _dataContext.Products.FindAsync(Id);
+            if(product != null) {
+                return new EProduct(product);
+            } else {
+                return new EProduct();
+            }
+            
+        }
+
+        public Task<EProduct?> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
