ccae1aed2f0a6ab6d42c4c2f99f6849c75325517
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-23 22:02:06 -0400

    isValid method added and properties updated

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,10 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace WebService.Models {
     public class Product {
         public int Id { get; set; }
-        public String Name { get; set; }
-        public String Sku { get; set; }
+        public String? Name { get; set; }
+        public String? Sku { get; set; }
         
         [Column(TypeName = "decimal(18,4)")]
-        public decimal Price { get; set; }
+        public decimal Price { get; set; } = 1.0m;
+
+        public bool isValid() {
+            return !string.IsNullOrEmpty(Name);
+        }
     }
 }
