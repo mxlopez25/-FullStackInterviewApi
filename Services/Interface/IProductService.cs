51f9b779e2599cc9010a512102ad30305da2531e
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-23 22:02:24 -0400

    IProductService updated

--- Services/Interface/IProductService.cs
+++ Services/Interface/IProductService.cs
@@ -3,10 +3,10 @@ using WebService.Models.Entities;
 
 namespace WebService.Services.Interfaces {
     public interface IProductService {
-        Task<EProduct> Get(int Id);
-        Task<EProduct> All();
-        Task<EProduct> Create(Product product);
-        Task<EProduct> Update(Product product);
+        Task<EProduct?> Get(int Id);
+        Task<EProduct?> All();
+        Task<EProduct?> Create(Product product);
+        Task<EProduct?> Update(Product product);
         Task<Boolean> Delete(int Id);
     }
 }
