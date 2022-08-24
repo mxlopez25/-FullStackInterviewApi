53ab81ea57a15a6b84e700e758964b3137a82476
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-23 20:02:53 -0400

    IProductService interface added

---
+++ Services/Interface/IProductService.cs
@@ -0,0 +1,12 @@
+using WebService.Models;
+using WebService.Models.Entities;
+
+namespace WebService.Services.Interfaces {
+    public interface IProductService {
+        Task<EProduct> Get(int Id);
+        Task<EProduct> All();
+        Task<EProduct> Create(Product product);
+        Task<EProduct> Update(Product product);
+        Task<Boolean> Delete(int Id);
+    }
+}
